0cfe3d9a4860135e4afef409cf6db7ab937e9b42
git-username <git-email-address>
2014-08-05 21:52:35 -0400

    reset to loading yo.html

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 var app = angular.module('myApp', []);
 
-/*app.directive('yoYo', function(){
+app.directive('yoYo', function(){
 
 	return {
-
-		// template : '<strong>Yo!</strong>',
 
 		templateUrl : './yo.html',
-
-		transclude : true,
-
-		restrict : 'E',
-
-		replace : true
-
-	};
-
-});*/
-
-app.directive('optIn', function(){
-
-	return {
-
-		// template : '<strong>Yo!</strong>',
-
-		templateUrl : './opt-in.html',
 
 		transclude : true,
 
